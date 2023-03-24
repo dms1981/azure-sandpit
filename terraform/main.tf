de39af6973a2855367e88fd3e8ea77b0761923ef
David Sibley <david.sibley@digital.justice.gov.uk>
2023-03-24 14:11:25 +0000

    added some simple content so that I could get a tfplan to cache between environments

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,14 @@ resource "azurerm_resource_group" "main" {
     environment = terraform.workspace
   }
 }
+
+resource "azurerm_virtual_network" "main" {
+  address_space       = ["10.0.0.0/16"]
+  location            = "UK South"
+  name                = format("vnet-dms1981-%s", terraform.workspace)
+  resource_group_name = azurerm_resource_group.main.name
+  tags     = {
+    owner = "david.sibley@digital.justice.gov.uk"
+    environment = terraform.workspace
+  }
+}
